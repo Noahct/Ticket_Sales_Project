6de5cadd90ce04b1d51a2ee04b047016b2acf2cc
“Noahct” <richtersckale@gmail.com>
2018-03-29 20:46:07 -0400

    initial commit.

---
+++ ticketmaneger.h
@@ -0,0 +1,16 @@
+//
+// ticketmaneger.h
+// Noah Tucker
+// This program will
+
+// This is start of the header guard.  ticketmaneger_h can be any unique name.  By convention, you use the name of the header file.
+#ifndef ticketmaneger_h
+#define ticketmaneger_h
+
+// This is the content of the .h file, which is where the declarations go
+class TicketManeger{
+}
+
+
+// This is the end of the header guard
+#endif
---
+++ ticketsales.cpp
@@ -0,0 +1,14 @@
+// Group Project
+// ticketsales.cpp
+// Noah Tucker and 
+// This program will
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+
+}
