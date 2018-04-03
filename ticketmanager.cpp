e7f2269459e97e8e98f0b754d370d6dc7922f16b
BlackBeardEC <rich.stadnick@gmail.com>
2018-04-03 19:37:52 -0400

    add parentheses for member functions

--- ticketmanager.cpp
+++ ticketmanager.cpp
@@ -8,22 +8,22 @@ TicketManager::TicketManager(){ //load files create 2-D array of seat structs
 
 }
 
-string TicketManager::displayChart{  //represent seats by a chart of * and # chars
+string TicketManager::displayChart(){  //represent seats by a chart of * and # chars
 
 }
 
-double TicketManager::ticketRequest{ //determine if requested seats are available and give the seating price
+double TicketManager::ticketRequest(){ //determine if requested seats are available and give the seating price
 
 }
-void TicketManager::purchase{ //purchase seats update date accordingly
-
-}
-
-void TicketManager::report{ //display sales report from salesReport.dat
+void TicketManager::purchase(){ //purchase seats update date accordingly
 
 }
 
-TicketManager::~TicketManager{ //write and close files
+void TicketManager::report(){ //display sales report from salesReport.dat
+
+}
+
+TicketManager::~TicketManager(){ //write and close files
 
 }
 
