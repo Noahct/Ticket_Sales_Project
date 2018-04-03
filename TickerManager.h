ae65dae67fffde4c6452dea06d220a2e2f76033a
derekmorgan38 <37957571+derekmorgan38@users.noreply.github.com>
2018-04-03 08:52:38 -0400

    Create TickerManager.h

---
+++ TickerManager.h
@@ -0,0 +1,9 @@
+1.	//insertion_Ticketmanager.h file
+2.	#ifndef _TicketManager_h
+3.	#define _TicketManager_h
+4.	 
+5.	/*Sorts an integer array. Takes a pointer to the first element and the length of the array as input. 
+6.	Returns 0 on successful sort.*/
+7.	int TicketManager(int*, int); 
+8.	 
+9.	#endif
