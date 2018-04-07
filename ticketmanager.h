94bcbd06b4d8386db9d04f8eb00f99942097c625
BlackBeardEC <rich.stadnick@gmail.com>
2018-04-07 18:00:49 -0400

    Move Seat struct into header, move Seat array declaration into header as private member.

--- ticketmanager.h
+++ ticketmanager.h
@@ -12,17 +12,21 @@
 using namespace std;
 const int ROWS = 15;
 const int COLS = 30;
+struct Seat{
+    double price;
+    bool available;
+};
 // This is the content of the .h file, which is where the declarations go
 class TicketManager{
     private:
-    
+        Seat seats[ROWS][COLS]; 
     public:
-    TicketManager(); //load files create 2-D array of seat structs
-	string displayChart();  //represent seats by a chart of * and # chars
-	double ticketRequest(); //determine if requested seats are available and give the seating price
-	void purchase(); //purchase seats update date accordingly
-	void report(); //display sales report from salesReport.dat
-	~TicketManager(); //write and close files
+        TicketManager(); //load files create 2-D array of seat structs
+        string displayChart();  //represent seats by a chart of * and # chars
+        bool ticketRequest(int num_seats, int row_num, int start_seat); //determine if requested seats are available and give the seating price
+        void purchase(); //purchase seats update date accordingly
+        void report(); //display sales report from salesReport.dat
+        ~TicketManager(); //write and close files
 
 };
 // This is the end of the header guard
