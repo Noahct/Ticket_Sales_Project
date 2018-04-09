9b57193d70027c98eb447ce39af03434daa0b6ea
“Noahct” <richtersckale@gmail.com>
2018-04-09 16:48:54 -0400

    fixed some bugs in ticketsales.cpp

--- ticketsales.cpp
+++ ticketsales.cpp
@@ -13,44 +13,48 @@ int getInput(int, int);  //declare getInput func
 
 int main(){
     //create var of type TicketManeger
-    TicketManeger manager();  //will not work until class constructor hes been created
+    TicketManeger manager;  //will not work until class constructor hes been created
 
     while (true){
         //display menu of options with cout
         cout << "menu items:\n" << "1) display seats\n" << "2) request tickets\n" << "3) sales report\n" << "4) quit\n";
 
         //get and validate user input with a function
-        menuItem = getInput(1, 4);
+        int menuItem = getInput(1, 4);
 
         if (menuItem == 1)  
             manager.displayChart();  //use the displayChart() method in the TicketManeger class
 
         if (menuItem == 2){  //ticketRequest purchase 
-            bool status = false;  //do seats exist and are they available
+            //bool status = false;  //do seats exist and are they available
             string mesg;
+            string buy;
+            double money;
             while (true){
                 int numseats = getInput(0, 30);
                 int row = getInput(0, 15);
                 int startseat = getInput(0, 30);
-                mesg, status = manager.ticketRequest(numseats, row, startseat);
-                cout << mesg << '\n';
-                if (not status)
+                double price = manager.ticketRequest(numseats, row, startseat);
+                if (price != -1){  //a price of -1 indicates that seats don't exist or are not available
+                    cout << "good message" << '\n';
                     cout << "Would you like to buy these seats?" << '\n';
                     cin >> buy;
                     if (buy == "y" or buy == "yes"){
                         do{ //get input until user provides enough money to buy seats
-                            double money = getInput();
+                            money = getInput(0, price);
                         }while (not manager.purchase(money));
                     }else{
+                        cout << "bad message" << '\n';
                         break;
+                    }
                 }
             }
         }
 
         if (menuItem == 3)  
-            manager.report()  //get sales report 
+            manager.report()  //display a sales report 
 
-        if (menuItem == 4)
+        if (menuItem == 4)  //quit the program
             return 0;
 	}
 }
